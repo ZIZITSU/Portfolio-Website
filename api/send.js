975a93ab33c9e98577fa2c9d687eb5739d3340e1
Mahir Labib <mahirlabib389@gmail.com>
2026-08-05 01:27:56 +0600

    trigger: force Vercel build with RESEND_API_KEY

--- api/send.js
+++ api/send.js
@@ -1,3 +1,4 @@
+// Resend API email handler - Vercel Serverless Function
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
